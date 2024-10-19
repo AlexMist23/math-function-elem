8e93c4afc9bdb83a1accf0f9bdb81108674a50f4
Alexander <aleksander.misterkiewicz@outlook.com>
2024-10-19 21:19:16 +0200

    landing page main component changed to ComplexFunctionDisplay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import LinearFunctionDisplay from "@/components/linear-function-display";
+import ComplexFunctionDisplay from "@/components/function/complex-function-display";
 
 export default function LinearFunctionPage() {
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-3xl font-bold mb-6">Linear Function Display</h1>
-      <LinearFunctionDisplay />
+      <ComplexFunctionDisplay />
     </div>
   );
 }
